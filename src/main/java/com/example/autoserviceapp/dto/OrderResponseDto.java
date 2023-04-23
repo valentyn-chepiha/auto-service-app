f137fc9b9ca4916505ceb94c26360daa7f101063
Valentyn Chepiha <valentyn.chepiha@gmail.com>
2023-04-23 23:36:56 +0300

    Added dto's and mapper's for Order

---
+++ src/main/java/com/example/autoserviceapp/dto/OrderResponseDto.java
@@ -0,0 +1,20 @@
+package com.example.autoserviceapp.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class OrderResponseDto {
+    private Long id;
+    private Long carId;
+    private String description;
+    private LocalDate dateStart;
+    private List<Long> operationIds;
+    private List<Long> detailIds;
+    private String statusName;
+    private BigDecimal total;
+    private LocalDate dateEnd;
+    private Long ownerId;
+}
---
+++ src/main/java/com/example/autoserviceapp/dto/mapper/impl/OrderResponseMapper.java
@@ -0,0 +1,32 @@
+package com.example.autoserviceapp.dto.mapper.impl;
+
+import java.util.stream.Collectors;
+import com.example.autoserviceapp.dto.OrderResponseDto;
+import com.example.autoserviceapp.dto.mapper.ResponseMapper;
+import com.example.autoserviceapp.model.Detail;
+import com.example.autoserviceapp.model.Operation;
+import com.example.autoserviceapp.model.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderResponseMapper implements ResponseMapper<Order, OrderResponseDto> {
+    @Override
+    public OrderResponseDto toDto(Order entity) {
+        OrderResponseDto dto = new OrderResponseDto();
+        dto.setId(entity.getId());
+        dto.setCarId(entity.getCar().getId());
+        dto.setDescription(entity.getDescription());
+        dto.setDateStart(entity.getDateStart());
+        dto.setOperationIds(entity.getOperations().stream()
+                .map(Operation::getId)
+                .collect(Collectors.toList()));
+        dto.setDetailIds(entity.getDetails().stream()
+                .map(Detail::getId)
+                .collect(Collectors.toList()));
+        dto.setStatusName(entity.getStatus().getName());
+        dto.setTotal(entity.getTotal());
+        dto.setDateEnd(entity.getDateEnd());
+        dto.setOwnerId(entity.getOwner().getId());
+        return dto;
+    }
+}
